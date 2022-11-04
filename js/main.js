509d7b8dbc1727b6031eaf83220a8e7ed13c47fa
luciobo <luciobo@icloud.com>
2022-11-04 16:34:25 +0100

    modifiche css

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+const immages = ["imgs/01.webp", "imgs/02.webp", "imgs/03.webp", "imgs/04.webp", "imgs/05.webp",]
+console.log(immages)
+
+
+const sliderImgEl = document.querySelector(".slider-img");
+const btnNext = document.getElementById("slider-btn-next");
+const btnPrev = document.getElementById("slider-btn-prev");
+
+
+
+for (let i = 0; i < immages.length; i++) {
+
+}
