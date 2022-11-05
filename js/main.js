3f0a60e37b137ed53b19e855d174c4b59c6e2622
luciobo <luciobo@icloud.com>
2022-11-05 15:17:22 +0100

    iniziata crazione slider

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 
-const immages = ["imgs/01.webp", "imgs/02.webp", "imgs/03.webp", "imgs/04.webp", "imgs/05.webp"]
-console.log(immages)
+const immages = [
+    "imgs/01.webp",
+    "imgs/02.webp",
+    "imgs/03.webp",
+    "imgs/04.webp",
+    "imgs/05.webp"
 
+];
+console.log(immages);
+
+
+let currentImgIndex = 0;
 
 const sliderImgEl = document.querySelector(".slider-img");
-const btnNext = document.getElementById("slider-btn-next");
-const btnPrev = document.getElementById("slider-btn-prev");
 
-const sliderContainer = document.querySelector(".slider-container");
+sliderImgEl.src = immages[currentImgIndex];
 
 
-
-
-for (let i = 0; i < immages.length; i++) {
-
-}
+const btnAvanti = document.getElementById("slider-btn-avanti");
+const btnIndietro = document.getElementById("slider-btn-indietro");
 
 
 
+btnAvanti.addEventListener("click", function () {
+    console.log("bottone avanti cliccato")
+
+})
 
 
+btnIndietro.addEventListener("click", function () {
+    console.log("bottone indietro cliccato")
 
-
-// btnNext.addEventListener("click", function () {
-
-// })
-
-
-// btnPrev.addEventListener("click", function () {
-    
-// })
+})
